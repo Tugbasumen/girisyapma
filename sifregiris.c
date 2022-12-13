948216db522302ebb4a7961514bd750324ed51df
Tugbasumen <119933049+Tugbasumen@users.noreply.github.com>
2022-12-13 10:18:47 +0300

    Add files via upload

---
+++ sifregiris.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	
+	int sifre=123;
+	char  kulanici_ad='t';
+	int hak=3;
+	
+	printf("***Giris Yapalim*** \n\n");
+	
+	while(1==1)
+	{
+		printf("adinizin bas harfini  giriniz: ");
+		scanf("%s",&kulanici_ad);
+		printf("sifrenizi giriniz:");
+		scanf("%d",&sifre);
+		printf("\n");
+		
+		if(kulanici_ad=='t' && sifre==123)
+		{
+			printf("TEBRÝKLER BASARÝLÝ GÝRÝS \n ");
+			break;
+		}
+		else
+		{
+			printf("sifre veya kullanici adi yanlis !! \n\n");
+			if(hak>0)
+			{
+				hak-=1;
+			}
+			if(hak==0)
+			{
+				printf("hakkiniz dolmustur \n\n");
+				
+				break;
+				
+			
+			}
+		}
+	}
+	
+	return 0;
+}
